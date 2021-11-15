eeb5a19967ded5342e0638e9b80c619ef4957060
Eric5F <ericsouza17@hotmail.com>
2021-11-15 19:34:28 -0300

    Novo Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.querySelector(".hamburguer").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
+document.querySelector(".container").classList.toggle("show-menu")
 );
 
 
@@ -9,49 +9,50 @@ function FundoQuad(){
 const ulSquares = document.querySelector("ul.squares");
 
 for (let i = 0; i < 20; i++){
-    const li = document.createElement("li");
+const li = document.createElement("li");
 
-    const random = (min, max) => Math.random() * (max - min) + min;
+const random = (min, max) => Math.random() * (max - min) + min;
 
-    const size = Math.floor(random(10, 80));
-    const position = random(1, 80);
-    const delay = random(5,  0.1);
-    const duration = random(24, 12);
+const size = Math.floor(random(10, 80));
+const position = random(1, 80);
+const delay = random(5,  0.1);
+const duration = random(24, 12);
 
-    li.style.width = `${size}px`;
-    li.style.height = `${size}px`;
-    li.style.bottom = `-${size}px`;
+li.style.width = `${size}px`;
+li.style.height = `${size}px`;
+li.style.bottom = `-${size}px`;
 
-    li.style.left = `${position}%`;
+li.style.left = `${position}%`;
 
-    li.style.animationDelay = `${delay}s`;
-    li.style.animationDuration = `${duration}s`
-    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
+li.style.animationDelay = `${delay}s`;
+li.style.animationDuration = `${duration}s`
+li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
 
-    ulSquares.appendChild(li);
+ulSquares.appendChild(li);
 };
+
 }
 
 
-    function SelectionH2(){
+function SelectionH2(){
 
-        const slides = document.querySelectorAll('.slides');
-        const dots = document.querySelectorAll('.dot');
-        
-        function setActive(i){
-            for(slide of slides)
-            slide.classList.remove('active');
-            slides[i].classList.add('active');
-        
-            for(dot of dots)
-            dot.classList.remove('active');
-            dots[i].classList.add('active');
-        }
-        for (let j = 0; j<dots.length; j++){
-            dots[j].addEventListener('click', function(){
-                setActive(j)
-            })
-        }}
+    const slides = document.querySelectorAll('.slides');
+    const dots = document.querySelectorAll('.dot');
+    
+    function setActive(i){
+        for(slide of slides)
+        slide.classList.remove('active');
+        slides[i].classList.add('active');
+    
+        for(dot of dots)
+        dot.classList.remove('active');
+        dots[i].classList.add('active');
+    }
+    for (let j = 0; j<dots.length; j++){
+        dots[j].addEventListener('click', function(){
+            setActive(j)
+        })
+    }}
 
     function Mensagem(){
         alert("Mensagem enviada com Sucesso");
